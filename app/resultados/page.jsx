85abd41bae5dddfe987d040197b32f002d9473b3
LuisBrodely <luisbrodely2@gmail.com>
2023-06-22 00:12:26 -0600

    first commit

--- app/resultados/page.jsx
+++ app/resultados/page.jsx
@@ -36,7 +36,7 @@ const posts = [
 export default function Resultados() {
   return (
     <>
-      <div className="relative bg-gray-50 pt-16 pb-20 lg:px-8 lg:pt-24 lg:pb-28">
+      <div className="relative bg-gray-50 pt-12 pb-20 lg:px-8 lg:pt-24 lg:pb-28">
         <div className="absolute inset-0">
           <div className="h-1/3 bg-white sm:h-2/3" />
         </div>
